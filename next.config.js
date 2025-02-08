b199280c7748400f2b031393e5e175941be97b5b
Olivia <mannerolivia@gmail.com>
2025-02-08 20:48:26 +0100

    Addition of homepage in package.json

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: [],
+    unoptimized: true, // Si tu utilises des images locales sans un backend
   },
 };
 
 module.exports = nextConfig;
+
+
+// output: 'export' est utile pour un déploiement sans backend.
+// trailingSlash: true permet d'éviter certains problèmes de navigation.
+// images.unoptimized: true est utile si tu utilises next/image mais sans backend.
